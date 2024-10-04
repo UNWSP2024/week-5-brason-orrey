5eb6ab5f30cf7407526ce958dec7e59618fff723
brason-orrey <borrey54@gmail.com>
2024-10-04 16:00:26 -0500

    Update program_1.py
    Brason Orrey - Submission Week 5

--- program_1.py
+++ program_1.py
@@ -1,26 +1,12 @@
-# Program #1: Kilometer Converter
-# Write a program that asks the user to enter a distance in kilometers, 
-# then converts that distance to miles.  The conversion formula is as follows:  
-# Miles = kilometers x 0.6214.   
-# The conversion must be done as a function with input and output.
-
-
-def kilometer_conversion(kilometers):    
-    miles = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################    
-
-
-    # Return the variable to the calling function
+def kilometers_to_miles(kilometers):
+    miles = kilometers * 0.6214
     return miles
 
-#### This piece of the code has been done for you,
-#### you only need to worry about the actual temp 
-#### conversion logic in the temp_conversion function
-if __name__ == '__main__':
-    # Get User Input
-    print('in main')
-    # Call kilometer_conversion
+def main():
+    kilometers = float(input("Enter distance in kilometers: "))
     
-    # Display the miles
+    miles = kilometers_to_miles(kilometers)
+    
+    print(f"{kilometers} kilometers is equal to {miles:.2f} miles.")
+
+main()
